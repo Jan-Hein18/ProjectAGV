44c322c6114b58d9fbfc6c49a859523c9c5ed9b9
victorzandbergen <victorzandbergen01@gmail.com>
2025-05-26 21:49:45 +0200

    lampjes toegevoegd
    Begin gemaakt aan lampjes, niet af want kwam wat tussen

---
+++ lampjes.c
@@ -0,0 +1,26 @@
+#include <IRSensor.c>
+
+
+#define LAMPREGISTER DDRB
+#define LAMPPORT PORTB
+#define LAMPGROEN PB6
+#define LAMPGEEL PB5
+
+void initLamp(void);{
+    LAMPREGISTER &= ~((1<<LAMPGEEL) | (1<<LAMPGROEN));
+    LAMPPORT &= ~((1<<LAMPGEEL) | (1<<LAMPGROEN));
+}
+
+void lampjesToggle(void){
+    int groenAan = 0;
+    int geelAan = 0;
+    if((IRSensorActief==1) && (geelAan == 0)){
+        geelAan = 1;
+        LAMPPORT |= (1<<LAMPGEEL)
+    }
+   if((IRSensorActief==0) && (geelAan == 1)){
+        geelAan = 0;
+        LAMPPORT &= ~(1<<LAMPGEEL)
+   }
+
+}
